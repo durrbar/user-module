135bd12d202326a88065dc7bd601d4bff8e44e17
Kid Max <kidmax285@gmail.com>
2026-03-28 17:33:23 +0600

    User model relation update

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,24 +1,42 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\User\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Contracts\Translation\HasLocalePreference;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use Modules\Address\Models\Address;
+use Modules\Chat\Models\Conversation;
+use Modules\Coupon\Models\Coupon;
+use Modules\Ecommerce\Models\Faqs;
+use Modules\Ecommerce\Models\Question;
+use Modules\Ecommerce\Models\TermsAndConditions;
+use Modules\Ecommerce\Models\Wallet;
 use Modules\Notification\Traits\HasNotification;
+use Modules\Order\Enums\OrderStatus;
+use Modules\Order\Models\Order;
+use Modules\Order\Models\OrderedFile;
+use Modules\Payment\Models\PaymentGateway;
+use Modules\Review\Models\Review;
 use Modules\User\Traits\HasProfileAvatar;
+use Modules\Vendor\Models\Shop;
 use Spatie\Permission\Traits\HasRoles;
 
 // use Modules\User\Database\Factories\UserFactory;
 
-class User extends Authenticatable implements HasLocalePreference, MustVerifyEmail
+final class User extends Authenticatable implements HasLocalePreference, MustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
@@ -37,6 +55,7 @@ class User extends Authenticatable implements HasLocalePreference, MustVerifyEma
     protected $appends = [
         'avatar_url',
         'name',
+        'email_verified',
     ];
 
     /**
@@ -66,23 +85,10 @@ class User extends Authenticatable implements HasLocalePreference, MustVerifyEma
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function getEmailVerifiedAttribute(): bool
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasVerifiedEmail();
     }
-
-    // protected static function newFactory(): UserFactory
-    // {
-    //     // return UserFactory::new();
-    // }
 
     /**
      * Return the full name of the customer.
@@ -91,13 +97,123 @@ class User extends Authenticatable implements HasLocalePreference, MustVerifyEma
      */
     public function getNameAttribute()
     {
-        return trim(
+        return mb_trim(
             preg_replace(
                 '/\s+/',
                 ' ',
                 "{$this->first_name} {$this->last_name}"
             )
         );
+    }
+
+    public function address(): HasMany
+    {
+        return $this->hasMany(Address::class, 'customer_id');
+    }
+
+    public function conversations(): HasMany
+    {
+        return $this->hasMany(Conversation::class, 'user_id');
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class, 'customer_id')->with(['products.variation_options', 'reviews']);
+    }
+
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class, 'customer_id');
+    }
+
+    public function wallet(): HasOne
+    {
+        return $this->hasOne(Wallet::class, 'customer_id');
+    }
+
+    public function shops(): HasMany
+    {
+        return $this->hasMany(Shop::class, 'owner_id');
+    }
+
+    public function refunds(): HasMany
+    {
+        return $this->hasMany(Shop::class, 'customer_id');
+    }
+
+    public function managed_shop(): BelongsTo
+    {
+        return $this->belongsTo(Shop::class, 'shop_id');
+    }
+
+    public function providers(): HasMany
+    {
+        return $this->hasMany(Provider::class, 'user_id', 'id');
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class, 'user_id');
+    }
+
+    public function questions(): HasMany
+    {
+        return $this->hasMany(Question::class, 'user_id');
+    }
+
+    public function ordered_files(): HasMany
+    {
+        return $this->hasMany(OrderedFile::class, 'customer_id');
+    }
+
+    /**
+     * Follow shop
+     */
+    public function follow_shops(): BelongsToMany
+    {
+        return $this->belongsToMany(Shop::class, 'user_shop');
+    }
+
+    /**
+     * Follow shop
+     */
+    public function payment_gateways(): HasMany
+    {
+        return $this->HasMany(PaymentGateway::class, 'user_id');
+    }
+
+    /**
+     * faqs
+     */
+    public function faqs(): HasMany
+    {
+        return $this->HasMany(Faqs::class);
+    }
+
+    /**
+     * terms and conditions
+     */
+    public function terms_and_conditions(): HasMany
+    {
+        return $this->HasMany(TermsAndConditions::class);
+    }
+
+    /**
+     * coupons
+     */
+    public function coupon(): HasMany
+    {
+        return $this->HasMany(Coupon::class);
+    }
+
+    public function loadLastOrder()
+    {
+        $data = $this->orders()->whereNull('parent_id')
+            ->where('order_status', OrderStatus::COMPLETED)
+            ->latest()->first();
+        $this->setRelation('last_order', $data);
+
+        return $this;
     }
 
     /**
@@ -124,5 +240,32 @@ class User extends Authenticatable implements HasLocalePreference, MustVerifyEma
     public function addresses()
     {
         return $this->hasMany(Address::class, 'created_by');
+    }
+
+    // protected static function newFactory(): UserFactory
+    // {
+    //     // return UserFactory::new();
+    // }
+
+    protected static function boot()
+    {
+        parent::boot();
+        // Order by updated_at desc
+        self::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('updated_at', 'desc');
+        });
+    }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
